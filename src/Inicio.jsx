b46c6cc5c09cf44c8208359d4083d69d7279bd38
Roberto <robertoz1777@gmail.com>
2025-02-07 20:54:25 +0100

    Inicio + bonito

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -7,25 +7,27 @@ export default function Inicio() {
       style={{ backgroundImage: 'url("/path/to/background-image.jpg")' }}
     >
       <header className="landing-header">
-        <h1 className="landing-title">¡Bienvenido a la PokeApi !</h1>
+        <h1 className="landing-title">¡Bienvenido a la PokeApi!</h1>
         <p className="landing-subtitle">
           Regístrate y prueba nuestro juego interactivo de Adivinar Pokémons con pistas y sonidos.
         </p>
       </header>
       <div className="video-container">
-  <video autoPlay loop muted className="wallpaper">
-    <source src="/videos/wallpaper.mp4" type="video/mp4" />
-    Tu navegador no soporta el elemento de video.
-  </video>
-</div>
+        <video autoPlay loop muted className="wallpaper">
+          <source src="/gengar.mp4" type="video/mp4" />
+          Tu navegador no soporta el elemento de video.
+        </video>
+      </div>
 
       <section className="landing-content">
         <div className="landing-feature">
           <h3>Explora la Pokedex</h3>
-          <p>Descubre las características de todos los Pokemons hasta la novena generacion.</p>
+          <p>Descubre las características de todos los Pokemons hasta la novena generación.</p>
           <button
             className="cta-button"
-            onClick={() => (window.location.href = "/pokemons")}
+            onClick={() => {
+              window.location.href = "/pokemons";
+            }}
           >
             Pokedex
           </button>
@@ -36,4 +38,3 @@ export default function Inicio() {
     </div>
   );
 }
-
